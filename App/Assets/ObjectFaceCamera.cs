588786c82086072461bfb259df332a49ddedaa25
colem <colem@iastate.edu>
2020-10-20 13:35:46 -0500

    ObjectFaceCamera

---
+++ App/Assets/ObjectFaceCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class ObjectFaceCamera : MonoBehaviour
+{
+    GameObject[] ARObjects;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.FindGameObjectsWithTag("ARObject");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(GameObject obj in ARObjects)
+        {
+            
+        }
+    }
+}
