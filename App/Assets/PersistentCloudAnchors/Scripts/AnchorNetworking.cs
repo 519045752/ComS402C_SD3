5ab9df0c769a63b89ebc8dc389cec03b968a51b1
aphares <aphares@iastate.edu>
2020-10-24 20:08:45 -0500

    add json parser

--- App/Assets/PersistentCloudAnchors/Scripts/AnchorNetworking.cs
+++ App/Assets/PersistentCloudAnchors/Scripts/AnchorNetworking.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.Networking;
+using System.Collections.Generic;
 
 
 public class AnchorNetworking : MonoBehaviour
@@ -14,27 +15,10 @@ public class AnchorNetworking : MonoBehaviour
     {
     }
 
-    public void getCloudIds()
-    {
-        request = UnityWebRequest.Get(baseurl + "all");
-        Debug.Log(request.downloadHandler.text);
-    }
-
-    // example POST: http://coms-402-sd-8.cs.iastate.edu:8080/arObject/add?cloudid=testid&description=this%20is%20some%20text&hid=1&type=0
-    public void AddCloudID(string cloudid, string desc, int hid, int type)
-    {
-        // creating local variables, as these values are reset
-        string id = cloudid;
-        string description = desc;
-        int house = hid;
-        int t = type;
-        StartCoroutine(Upload(id, description, house, t));
-    }
-
-    public IEnumerator Upload(string cloudid, string desc, int hid, int type)
+    public IEnumerator AddCloudID(string cloudid, string desc, int hid, int type)
     {
 
-        Debug.Log("Begin Upload");
+        Debug.Log("Begin AddCloudID");
         WWWForm form = new WWWForm();
         form.AddField("cloudid", cloudid);
         form.AddField("description", desc);
@@ -53,6 +37,31 @@ public class AnchorNetworking : MonoBehaviour
             else
             {
                 Debug.Log("ARObject: Form upload complete!");
+            }
+        }
+    }
+
+    public IEnumerator getCloudId(List<string> cloudids)
+    {
+
+        Debug.Log("Begin getCloudId");
+
+        // from https://docs.unity3d.com/ScriptReference/Networking.UnityWebRequest.Post.html
+        using (UnityWebRequest www = UnityWebRequest.Get(baseurl + "all"))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log("ARObject: result of get was --> \n " + www.downloadHandler.text);
+                for (int i = 0; i < 1; i++)
+                {
+                    
+                }
             }
         }
     }
